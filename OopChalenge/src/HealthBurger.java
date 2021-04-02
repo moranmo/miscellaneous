1d6e12c091c607cdb03112ead1a4d9b39d7c53b5
moranmo <moran.m78@gmail.com>
2021-04-02 22:38:04 +0300

    Add files via upload

---
+++ OopChalenge/src/HealthBurger.java
@@ -0,0 +1,9 @@
+
+public class HealthBurger extends Hamburger {
+
+	public HealthBurger(String meat, double Price) {
+		super("HealthyBurger", meat, Price, "brown brad");
+		// TODO Auto-generated constructor stub
+	}
+
+}
