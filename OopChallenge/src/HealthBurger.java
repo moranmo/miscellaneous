4e3a05ba6043028fc6b0c26aab659d667ba1ba2f
moranmo <moran.m78@gmail.com>
2021-04-23 21:35:29 +0300

    Delete OopChallenge directory

--- OopChallenge/src/HealthBurger.java
+++
@@ -1,9 +0,0 @@
-
-public class HealthBurger extends Hamburger {
-
-	public HealthBurger(String meat, double Price) {
-		super("HealthyBurger", meat, Price, "brown brad");
-		// TODO Auto-generated constructor stub
-	}
-
-}
